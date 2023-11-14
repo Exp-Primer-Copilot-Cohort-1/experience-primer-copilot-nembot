3da6e51e782893c991859521a5ba3a022ffe7d63
Stef <nguyen.am.stephane@gmail.com>
2023-11-14 08:53:21 +0100

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,9 @@
-function skillsmember() {
-    var member = document.getElementById("member");
-    var skills = document.getElementById("skills");
-    var memberbtn = document.getElementById("memberbtn");
-    var skillsbtn = document.getElementById("skillsbtn");
-    member.style.display = "block";
-    skills.style.display = "none";
-    memberbtn.style.backgroundColor = "#333";
-    memberbtn.style.color = "#fff";
-    skillsbtn.style.backgroundColor = "#fff";
-    skillsbtn.style.color = "#333";
+function skillsMember() {
+    return {
+        restrict: 'E',
+        scope: {
+            member: '='
+        },
+        templateUrl: 'skills-member.html'
+    };
 }
